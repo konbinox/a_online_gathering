bbf2700d8f6e2f2c9bec8ae7085fe98ac57e807f
konbinox <95138368+konbinox@users.noreply.github.com>
2026-05-02 09:15:41 -0400

    Change base path to relative in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/a_online_gathering/',  // 关键！改成你的仓库名
+  base: './',  // 改成 './' 使用相对路径
   plugins: [vue()]
 })
